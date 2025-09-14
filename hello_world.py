e396e0ca4a4eec3cc0b55a47b1521a4c021cc2ea
Zachary Tian <ztian360@gmail.com>
2025-09-14 13:34:54 -0500

    Added Hello World baseline

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+def main():
+    """Main function that prints Hello World message."""
+    print("Hello, World!")
+    print("Welcome to your Deep Learning Project!")
+
+if __name__ == "__main__":
+    main()
